fee3b96a6873e7dbdcc72aad9853b7112c3cf075
tfufuz1 <tfufuz@gmail.com>
2026-09-12 12:15:40 +0200

    Shell-Commit

--- crates/memfuse-db/tests/fusion_edge_cases_test.rs
+++ crates/memfuse-db/tests/fusion_edge_cases_test.rs
@@ -9,13 +9,13 @@
 //   3. Determinismus: Tie-breaking ist strikt deterministisch über doc_id.cmp().
 
 #[cfg(feature = "coherence-bonus-fusion")]
+use memfuse_db::fusion::ResonanceConfig;
+#[cfg(feature = "coherence-bonus-fusion")]
 use memfuse_db::fusion::{apply_resonance_bonus, ResonanceConfig};
 use memfuse_db::fusion::{
     build_provenance, reciprocal_rank_fusion, weighted_reciprocal_rank_fusion,
     weighted_reciprocal_rank_fusion_with_options, MetadataMergePriority,
 };
-#[cfg(feature = "coherence-bonus-fusion")]
-use memfuse_db::fusion::ResonanceConfig;
 use memfuse_db::SearchResult;
 use proptest::prelude::*;
 
